af2d2f37aa66d69bcdf15cd243229606b6a30987
Chuigda Whitegive <chuigda@doki7.club>
2026-09-13 21:57:18 +0800

    ~

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import { marked } from 'marked'
+import markedKatex from 'marked-katex-extension'
 import markedCjkFriendly from 'marked-cjk-friendly'
 import './style.css'
 import App from './App.vue'
 
+marked.use(markedKatex({
+  throwOnError: false,
+  displayMode: false
+}))
 marked.use(markedCjkFriendly())
 createApp(App).mount('#app')
