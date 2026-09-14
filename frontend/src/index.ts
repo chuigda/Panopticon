d8836dc964a233029245619f621d8918346ed4ee
Cousin Ze <alex@ckx-ily.cn>
2026-09-14 10:06:58 +0800

    ~

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import { marked } from 'marked'
-import markedKatex from 'marked-katex-extension'
 import markedCjkFriendly from 'marked-cjk-friendly'
+import markedKatex from 'marked-katex-extension'
 import 'katex/dist/katex.min.css'
 
 import App from './App.vue'
 import './style.css'
 
-marked.use(markedKatex({ throwOnError: false }))
 marked.use(markedCjkFriendly())
+marked.use(markedKatex({ throwOnError: false }))
 createApp(App).mount('#app')
